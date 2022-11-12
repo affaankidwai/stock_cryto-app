51c434cbc2ac9388f6dbba47e2dfb19a9d880aad
Affaan Kidwai <kidwaiaffaan@gmail.com>
2022-11-12 18:14:46 +0530

    first displayed

--- App.js
+++ App.js
@@ -6,8 +6,9 @@ import {
   TouchableOpacity,
   SafeAreaView,
   FlatList,
-  Image
+  Image,
 } from "react-native";
+import Coin from "./components/Coin";
 
 import React, { useState, useEffect } from "react";
 import { getMarketData } from "/Users/affaankidwai/Desktop/stock-crypto/services/cryptodata.jsx";
@@ -34,11 +35,11 @@ export default function App() {
         <Text style={{ color: `#487d8b`, fontSize: 28, marginBottom: "10%" }}>
           Affaan Kidwai
         </Text>
-        <StatusBar style="auto" />
+        {/* <StatusBar style="auto" /> */}
         {/* <Switch /> */}
       </View>
 
-      <View style={{ flex: 1, flexDirection: "row" }}>
+      {/* <View style={{ flex: 1, flexDirection: "row" }}>
         <TouchableOpacity
           style={styles.midBar}
           onPress={() => console.log("you pressed a text")}
@@ -63,47 +64,9 @@ export default function App() {
         >
           Wishlist
         </Text>
-      </View>
-
-      <View style={styles.coinInfo}>
-        <View style={styles.left}>
-          <Image
-            source={{
-              uri: "https://img.freepik.com/premium-vector/gold-coin-bitcoin-bitcoin-cryptocurrency-symbol-isolated-transparent-background_279501-2310.jpg?w=20",
-            }}
-          />
-          <View>
-            <Text>
-              Bitcoin
-            </Text>
-            <Text>
-              BTC
-            </Text>
-          </View>
-          
-        </View>
-
-        <View style={styles.right}>
-          <Text>Bitcoin</Text>
-          <Text>BTC</Text>
-        </View>
-
-        {/* <FlatList
-    keyExtractor={(item) => item.id}
-    data={data}
-    // renderItem={({ item }) => (
-    //   <ListItem
-    //     name={item.name}
-    //     // symbol={item.symbol}
-    //     // currentPrice={item.current_price}
-    //     // priceChangePercentage7d={item.price_change_percentage_7d_in_currency}
-    //     // logoUrl={item.image}
-    //     // onPress={() => openModal(item)}
-    //     onPress={"console.log()"}
-      
-    
-  /> */}
-      </View>
+      </View> */}
+      <View style={styles.divider} />
+      <Coin />
     </SafeAreaView>
   );
 }
@@ -112,8 +75,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
+    // alignItems: "flex-start",
+    // justifyContent: "flex-start",
   },
   topText: {
     paddingTop: "20%",
@@ -123,16 +86,12 @@ const styles = StyleSheet.create({
     padding: "3%",
     fontSize: 19,
   },
-  coinInfo: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-
+  left: {},
+  right: {},
+  divider: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: "#A9ABB1",
+    marginHorizontal: 16,
+    marginTop: 16,
   },
-  left: {
-
-  },
-  right: {
-
-  }
 });
--- components/Coin.jsx
+++ components/Coin.jsx
@@ -1,86 +1,131 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  TouchableOpacity,
-  SafeAreaView,
-  
-} from "react-native";
-import React, { useState, useEffect } from "react";
-import { getMarketData } from '/Users/affaankidwai/Desktop/stock-crypto/services/cryptodata.jsx'
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import React from "react";
 
 const Coin = () => {
-  const [data, setData] = useState([]);
-//   const [selectedCoinData, setSelectedCoinData] = useState(null);
-
-  useEffect(() => {
-    const fetchMarketData = async () => {
-      const marketData = await getMarketData();
-      setData(marketData);
-    };
-    fetchMarketData();
-    //   console.log(response.data);
-    // });
-  }, []);
-
   return (
-    <SafeAreaView>
-        <View style={styles.container}>
-      <View style={styles.topText}>
-        <Text style={{ color: "#888", fontSize: 20, paddingBottom: "2%" }}>
-          Welcome Back!
-        </Text>
-        <Text style={{ color: `#487d8b`, fontSize: 28, marginBottom: '10%' }}>Affaan Kidwai</Text>
-        <StatusBar style="auto" />
-        {/* <Switch /> */}
-      </View>
-      
-      <View style={{ flex: 1, flexDirection: "row"}}>
-      <TouchableOpacity
-        style={styles.midBar}
-        onPress={() => console.log("you pressed a text")}
-      >
-        <Text>Trending</Text>
-      </TouchableOpacity>
-        <Text style={styles.midBar} onPress={() => console.log("you pressed a text")}>Top Stocks</Text>
-        <Text style={styles.midBar} onPress={() => console.log("you pressed a text")}>Top Crypto</Text>
-        <Text style={styles.midBar} onPress={() => console.log("you pressed a text")}>Wishlist</Text>
-      </View>
-      {/* <ScrollView style={{flex: 1, flexDirection: 'row'}}>
-        <Text>Hello</Text>
-
-      </ScrollView> */}
-      {/* <Coin /> */}
-    </View>
-        <View style={styles.wrapper}>
-      <FlatList
-        keyExtractor={(item) => item.id}
-        data={data}
-        renderItem={({ item }) => (
-          <ListItem
-            name={item.name}
-            // symbol={item.symbol}
-            // currentPrice={item.current_price}
-            // priceChangePercentage7d={item.price_change_percentage_7d_in_currency}
-            // logoUrl={item.image}
-            // onPress={() => openModal(item)}
-            onPress={"console.log()"}
+    <TouchableOpacity>
+      <View style={styles.coinInfo}>
+        <View style={styles.left}>
+          <Image
+            source={{
+              uri: "https://img.freepik.com/premium-vector/gold-coin-bitcoin-bitcoin-cryptocurrency-symbol-isolated-transparent-background_279501-2310.jpg?w=20",
+            }}
           />
-        )}
-        //     ListHeaderComponent={<ListHeader />}
-      />
-      </View>
+          <View>
+            <Text>Bitcoin</Text>
+            <Text>BTC</Text>
+          </View>
+        </View>
 
-    </SafeAreaView>
+        <View style={styles.right}>
+          <Text>Bitcoin</Text>
+          <Text>BTC</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
+export default Coin;
+
 const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-    backgroundColor: '000000'
+  coinInfo: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "E7E7E7",
+    borderRadius: "10px",
+    marginHorizontal: "4%",
+    marginTop: 15,
+    height: 60,
   },
+  left: {},
+  right: {},
 });
 
-export default Coin;
+// import {
+//   StyleSheet,
+//   Text,
+//   View,
+//   FlatList,
+//   TouchableOpacity,
+//   SafeAreaView,
+
+// } from "react-native";
+// import React, { useState, useEffect } from "react";
+// import { getMarketData } from '/Users/affaankidwai/Desktop/stock-crypto/services/cryptodata.jsx'
+
+// const Coin = () => {
+//   const [data, setData] = useState([]);
+// //   const [selectedCoinData, setSelectedCoinData] = useState(null);
+
+//   useEffect(() => {
+//     const fetchMarketData = async () => {
+//       const marketData = await getMarketData();
+//       setData(marketData);
+//     };
+//     fetchMarketData();
+//     //   console.log(response.data);
+//     // });
+//   }, []);
+
+//   return (
+//     <SafeAreaView>
+//         <View style={styles.container}>
+//       <View style={styles.topText}>
+//         <Text style={{ color: "#888", fontSize: 20, paddingBottom: "2%" }}>
+//           Welcome Back!
+//         </Text>
+//         <Text style={{ color: `#487d8b`, fontSize: 28, marginBottom: '10%' }}>Affaan Kidwai</Text>
+//         <StatusBar style="auto" />
+//         {/* <Switch /> */}
+//       </View>
+
+//       <View style={{ flex: 1, flexDirection: "row"}}>
+//       <TouchableOpacity
+//         style={styles.midBar}
+//         onPress={() => console.log("you pressed a text")}
+//       >
+//         <Text>Trending</Text>
+//       </TouchableOpacity>
+//         <Text style={styles.midBar} onPress={() => console.log("you pressed a text")}>Top Stocks</Text>
+//         <Text style={styles.midBar} onPress={() => console.log("you pressed a text")}>Top Crypto</Text>
+//         <Text style={styles.midBar} onPress={() => console.log("you pressed a text")}>Wishlist</Text>
+//       </View>
+//       {/* <ScrollView style={{flex: 1, flexDirection: 'row'}}>
+//         <Text>Hello</Text>
+
+//       </ScrollView> */}
+//       {/* <Coin /> */}
+//     </View>
+//         <View style={styles.wrapper}>
+//       <FlatList
+//         keyExtractor={(item) => item.id}
+//         data={data}
+//         renderItem={({ item }) => (
+//           <ListItem
+//             name={item.name}
+//             // symbol={item.symbol}
+//             // currentPrice={item.current_price}
+//             // priceChangePercentage7d={item.price_change_percentage_7d_in_currency}
+//             // logoUrl={item.image}
+//             // onPress={() => openModal(item)}
+//             onPress={"console.log()"}
+//           />
+//         )}
+//         //     ListHeaderComponent={<ListHeader />}
+//       />
+//       </View>
+
+//     </SafeAreaView>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   wrapper: {
+//     flex: 1,
+//     backgroundColor: '000000'
+//   },
+// });
+
+// export default Coin;
